0f8e4c8f2242f8a4959e387b9815a0df56dc2719
HackingTime <gcgithub@bar.edu>
2017-02-23 16:09:21 -0800

    changing input method

--- addfrac.c
+++ addfrac.c
@@ -5,10 +5,8 @@ int main()
 {
 
 int num1, num2, den1, den2, numrus, denrus;
-printf("First Fraction:");
-scanf("%d/%d", &num1, &den1);
-printf("\nSecond Fraction:");
-scanf("%d/%d", &num2, &den2);
+printf("Fraction (x/x + x/x):");
+scanf("%d/%d + %d/%d", &num1, &den1, &num2, &den2);
 
 //calc
 
